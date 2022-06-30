55bef1c8c7044783b4e02675c55c98a09bab1b67
gokhan <kemasgokhan@gmail.com>
2022-06-30 09:55:23 +0300

    ad

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -7,15 +7,17 @@ const Card = () => {
         <div className="cards-area-container">
      <div className="bars"></div>
       <h1 className="language-title">Languages</h1>
-      
+      <div className="cards-container">
+
             {languages.map((item,index) =>{
              return(
      
                <Item card={item} key={index} /> )}
              )
             }
-         )
+      </div>
         </div>
+         )
 }
 
 
