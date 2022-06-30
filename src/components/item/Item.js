79f861a69913f0faebdb051e5d12d44c6f3c7483
gokhan <kemasgokhan@gmail.com>
2022-06-30 10:01:23 +0300

    a

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,1 +1,15 @@
 import React from 'react';
+
+const Item =  ({image,name,option}) =>{
+ 
+    return(
+        <div className="card">
+       <img src="{image}" alt="" className="card-logo" />
+        <ul>
+
+        </ul>
+       
+        </div>
+    )
+
+}
