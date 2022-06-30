a812871db4375cbb3d18afd806672e46b8322d2c
gokhan <kemasgokhan@gmail.com>
2022-06-30 15:04:28 +0300

    ad

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,40 +1,32 @@
-import React, { useState } from 'react';
-import "./Item.css"
-const Item =  ({card}) =>{
- const{name,img,options} =card
- const [logo,setlogo]=useState(true)
- const handleClick= () =>{
-      setlogo(!logo)
- 
-         
-    
+import React, { useState } from "react";
+import "./Item.css";
+const Item = ({ card }) => {
+  const { name, img, options } = card;
+  const [logo, setlogo] = useState(true);
 
- }   
- return(
-        <div className="card" onClick={handleClick}>
-       {logo ? (
-
-    <div>
-       <img className="card-logo" src={img} alt="logo"  />
-          <h3 className='card-title'>{name}</h3>
-            </div>
-       ) : (
-
-        <ul className='list'>
-            {options.map((el,index)=>{
-                return(
-                    <li>{el}</li>
-                )
-            })}
-        </ul>
-
-
-       )}
-       
+  const keeptrue = () => {
+    setlogo(true);
+  };
+  const handleClick = () => {
+    setlogo(!logo);
+    setTimeout(keeptrue, 4000);
+  };
+  return (
+    <div className="card" onClick={handleClick}>
+      {logo ? (
+        <div>
+          <img className="card-logo" src={img} alt="logo" />
+          <h3 className="card-title">{name}</h3>
         </div>
-    )
-
-}
-
+      ) : (
+        <ul className="list">
+          {options.map((el, index) => {
+            return <li>{el}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Item;
