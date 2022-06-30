73a9ebde8095673b88eaad7576e3997f96b08bde
gokhan <kemasgokhan@gmail.com>
2022-06-30 09:27:54 +0300

    ad

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Header from './components/header/Header';
 function App() {
   return (
     <div className="App">
-    <h1>Language Card</h1>
-    <Header/>
+        <Header/>
+      <div className="bar"></div>
+        <h1>Language Card</h1>
+      <div className="card-container"></div>
     </div>
   );
 }
